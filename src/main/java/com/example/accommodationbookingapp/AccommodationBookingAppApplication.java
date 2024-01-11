c40046f672e8a927c038054f7ecf0d9cd0ac9447
Vadym Hurnik <vad.com.z@gmail.com>
2024-01-11 14:20:01 +0200

    init commit

---
+++ src/main/java/com/example/accommodationbookingapp/AccommodationBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.accommodationbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationBookingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccommodationBookingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accommodationbookingapp/AccommodationBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accommodationbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
