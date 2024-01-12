1bc2f54ba496f56f5065c4ef216809815db0d528
Dmytro Potapov <potapovds1993@gmail.com>
2024-01-12 17:28:51 +0200

    fix: implemented mapper

--- src/main/java/com/example/accommodationbookingapp/mapper/AccommodationMapper.java
+++ src/main/java/com/example/accommodationbookingapp/mapper/AccommodationMapper.java
@@ -2,14 +2,27 @@ package com.example.accommodationbookingapp.mapper;
 
 import com.example.accommodationbookingapp.config.MapperConfig;
 import com.example.accommodationbookingapp.dto.accommodation.AccommodationResponseDto;
-import com.example.accommodationbookingapp.dto.accommodation.CreateAccommodationRequestDto;
 import com.example.accommodationbookingapp.model.Accommodation;
+import com.example.accommodationbookingapp.model.Amenity;
+import java.util.stream.Collectors;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface AccommodationMapper {
+    @Mapping(target = "amenities", ignore = true)
     AccommodationResponseDto toResponseDto(Accommodation accommodation);
 
-    Accommodation toModel(CreateAccommodationRequestDto requestDto);
+    @AfterMapping
+    default void setAmenities(@MappingTarget AccommodationResponseDto responseDto,
+                              Accommodation accommodation) {
+        if (accommodation.getAmenities() != null) {
+            responseDto.setAmenities(accommodation.getAmenities().stream()
+                    .map(Amenity::getName)
+                    .collect(Collectors.toList()));
+        }
+    }
 }
 
