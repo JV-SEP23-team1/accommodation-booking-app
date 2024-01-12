c5b98e71ecfef6ebf53fe6caa9d3d6eaf2ac829e
Vadym Hurnik <vad.com.z@gmail.com>
2024-01-12 15:14:50 +0200

    added security with JWT

--- src/main/java/com/example/accommodationbookingapp/controller/AuthenticationController.java
+++ src/main/java/com/example/accommodationbookingapp/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.example.accommodationbookingapp.controller;
 
+import com.example.accommodationbookingapp.dto.user.UserLoginRequestDto;
+import com.example.accommodationbookingapp.dto.user.UserLoginResponseDto;
 import com.example.accommodationbookingapp.dto.user.UserRegistrationRequestDto;
 import com.example.accommodationbookingapp.dto.user.UserResponseDto;
 import com.example.accommodationbookingapp.exception.RegistrationException;
+import com.example.accommodationbookingapp.security.AuthenticationService;
 import com.example.accommodationbookingapp.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     @ResponseStatus(HttpStatus.CREATED)
@@ -28,5 +32,14 @@ public class AuthenticationController {
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         return userService.register(requestDto);
+    }
+
+    @PostMapping("/login")
+    @Operation(
+            summary = "Login using existing credentials",
+            description = "Login using existing credentials"
+    )
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
