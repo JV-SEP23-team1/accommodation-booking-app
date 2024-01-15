9fb6d31018eb74756b24370e3e0a2fe8ea4b3f74
Vadym Hurnik <49322702+vadymhrnk@users.noreply.github.com>
2024-01-15 15:07:43 +0200

    [Exception]: added CustomGlobalExceptionHandler and AuthenticationException (#10)
    * added CustomGlobalExceptionHandler and AuthenticationException
    
    * added EntityNotFoundException

--- src/main/java/com/example/accommodationbookingapp/security/AuthenticationService.java
+++ src/main/java/com/example/accommodationbookingapp/security/AuthenticationService.java
@@ -2,8 +2,10 @@ package com.example.accommodationbookingapp.security;
 
 import com.example.accommodationbookingapp.dto.user.UserLoginRequestDto;
 import com.example.accommodationbookingapp.dto.user.UserLoginResponseDto;
+import com.example.accommodationbookingapp.exception.AuthenticationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -11,18 +13,25 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class AuthenticationService {
+    private static final String LOGIN_ERROR_MESSAGE = "Can't log in: ";
+
     private final JwtUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
 
-    public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto) {
-        final Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        requestDto.getEmail(),
-                        requestDto.getPassword()
-                )
-        );
+    public UserLoginResponseDto authenticate(UserLoginRequestDto requestDto)
+            throws AuthenticationException {
+        try {
+            final Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getEmail(),
+                            requestDto.getPassword()
+                    )
+            );
 
-        String token = jwtUtil.generateToken(authentication.getName());
-        return new UserLoginResponseDto(token);
+            String token = jwtUtil.generateToken(authentication.getName());
+            return new UserLoginResponseDto(token);
+        } catch (BadCredentialsException ex) {
+            throw new AuthenticationException(LOGIN_ERROR_MESSAGE, ex);
+        }
     }
 }
