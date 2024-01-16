5953786ab36e91fe024ea75524d7d7d045ad5dc9
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-01-16 15:19:18 +0200

    Added roles.

--- src/main/java/com/example/accommodationbookingapp/controller/PaymentController.java
+++ src/main/java/com/example/accommodationbookingapp/controller/PaymentController.java
@@ -6,11 +6,13 @@ import com.example.accommodationbookingapp.model.Payment;
 import com.example.accommodationbookingapp.service.payment.PaymentService;
 import com.example.accommodationbookingapp.service.url.UriService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,10 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/payments")
 @RequiredArgsConstructor
+@Tag(name = "Payment management", description = "Endpoints for managing payments")
 public class PaymentController {
     private PaymentService paymentService;
     private UriService uriService;
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/{id}")
     @Operation(summary = "Get Payments by User ID",
             description = "Get Payments as dto by User ID")
@@ -33,6 +37,7 @@ public class PaymentController {
         return paymentService.findAllByUserId(id);
     }
 
+    @PreAuthorize("hasRole('USER')")
     @PostMapping
     @Operation(summary = "Create new Payment",
             description = "Create new Payment using CreatePaymentDto")
@@ -48,6 +53,7 @@ public class PaymentController {
 
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/success")
     @Operation(summary = "Updated Payment status in case of successful payment",
             description = "Updated Payment status to PAID in case of successful payment")
@@ -55,6 +61,7 @@ public class PaymentController {
         return paymentService.update(sessionId, Payment.Status.PAID);
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/cancel")
     @Operation(summary = "Updated Payment status in case of canceled payment",
             description = "Updated Payment status to CANCELED in case of canceled payment")
