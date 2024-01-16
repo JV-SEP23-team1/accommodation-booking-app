7c683af7f3ee8f697b1bd7d44d0ed41879c17d98
Vadym Hurnik <vad.com.z@gmail.com>
2024-01-16 14:27:06 +0200

    finished UserControllerTest

--- src/test/java/com/example/accommodationbookingapp/controller/UserControllerTest.java
+++ src/test/java/com/example/accommodationbookingapp/controller/UserControllerTest.java
@@ -4,12 +4,22 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.accommodationbookingapp.dto.user.UserWithRolesResponseDto;
+import com.example.accommodationbookingapp.model.Role;
+import com.example.accommodationbookingapp.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Set;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -20,6 +30,8 @@ import org.springframework.web.context.WebApplicationContext;
 class UserControllerTest {
     protected static MockMvc mockMvc;
 
+    private static UserWithRolesResponseDto userWithRolesResponseDto;
+
     @Autowired
     private ObjectMapper objectMapper;
 
@@ -29,15 +41,49 @@ class UserControllerTest {
                 .webAppContextSetup(applicationContext)
                 .apply(springSecurity())
                 .build();
+
+        userWithRolesResponseDto = new UserWithRolesResponseDto();
+        userWithRolesResponseDto.setId(1L);
+        userWithRolesResponseDto.setEmail("john@example.com");
+        userWithRolesResponseDto.setFirstName("John");
+        userWithRolesResponseDto.setLastName("Doe");
+        userWithRolesResponseDto.setRoleNames(Set.of(Role.RoleName.USER));
+    }
+
+    @BeforeEach
+    void setUp() {
+        Role role = new Role();
+        role.setId(1L);
+        role.setRoleName(Role.RoleName.USER);
+
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("john@example.com");
+        user.setPassword("$2a$10$kmYQ3CgmYPzWNsK9q8oLF.VCoXCDeXqY2suHL2Z6GdnN18CtLkjTO");
+        user.setRoles(Set.of(role));
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                user, user.getPassword(), user.getAuthorities()
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     @WithMockUser(username = "user", roles = "USER")
     @Test
     public void getUser_ValidUser_Successful() throws Exception {
         MvcResult result = mockMvc.perform(
-                        get("/me").contentType(MediaType.APPLICATION_JSON)
+                        get("/users/me").contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().isOk())
                 .andReturn();
+
+        UserWithRolesResponseDto expected = userWithRolesResponseDto;
+
+        UserWithRolesResponseDto actual = objectMapper.readValue(
+                result.getResponse().getContentAsString(),
+                UserWithRolesResponseDto.class
+        );
+
+        Assertions.assertTrue(EqualsBuilder.reflectionEquals(expected, actual, "id"));
     }
 }
