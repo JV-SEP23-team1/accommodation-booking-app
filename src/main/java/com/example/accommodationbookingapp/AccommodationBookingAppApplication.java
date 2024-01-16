538b5398b79cd2cc704e147446b221b45360802a
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-01-16 14:17:38 +0200

    Setup stripeApiKey for the application.

--- src/main/java/com/example/accommodationbookingapp/AccommodationBookingAppApplication.java
+++ src/main/java/com/example/accommodationbookingapp/AccommodationBookingAppApplication.java
@@ -1,10 +1,20 @@
 package com.example.accommodationbookingapp;
 
+import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AccommodationBookingAppApplication {
+
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
+    @PostConstruct
+    public void setup() {
+        Stripe.apiKey = stripeApiKey;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AccommodationBookingAppApplication.class, args);
